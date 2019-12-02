f58ae61bb3ae3d8ff8fdd0f504c4e543ac40bc60
Filip Juric <wizehood@gmail.com>
2019-12-02 18:49:39 +0100

    remove logging comments

--- backend/startup/logging.js
+++ backend/startup/logging.js
@@ -1,7 +1,4 @@
 const winston = require('winston');
-// const config = require('config');
-// require('winston-mongodb');
-// require('express-async-errors');
 
 module.exports = function () {
     winston.configure({
@@ -28,11 +25,4 @@ module.exports = function () {
     process.on('unhandledRejection', (ex) => {
         throw ex;
     });
-
-    // winston.configure({ transports: [new winston.transports.File({ filename: 'logfile.log' })] });
-    // winston.add(winston.transports.File, { filename: 'logfile.log' });
-    // winston.add(winston.transports.MongoDB, {
-    //     db: config.get('db'),
-    //     level: 'info'
-    // });
 }
