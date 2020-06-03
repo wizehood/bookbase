7ef05bea118f5f2b81c4b350349f76196ac6c806
Filip Juric <wizehood@gmail.com>
2020-06-03 16:11:56 +0200

    upgrade packages + tweak logging

--- backend/startup/logging.js
+++ backend/startup/logging.js
@@ -1,9 +1,11 @@
 const winston = require('winston');
+const path = require('path')
 
 module.exports = function () {
     winston.configure({
         transports: [
             new winston.transports.File({
+                dirname: path.join(__dirname, 'logs'),
                 filename: 'logfile.log', format: winston.format.combine(
                     winston.format.timestamp(),
                     winston.format.json()
@@ -13,6 +15,7 @@ module.exports = function () {
         ],
         exceptionHandlers: [
             new winston.transports.File({
+                dirname: path.join(__dirname, 'logs'),
                 filename: 'uncaughtExceptions.log', format: winston.format.combine(
                     winston.format.timestamp(),
                     winston.format.json()
